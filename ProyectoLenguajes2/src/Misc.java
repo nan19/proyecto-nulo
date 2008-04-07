375703f14e63f72560312b1f376603b0113ae408
jamilnavarro <jamilnavarro@55ee1ba8-1b47-0410-a318-bdcbb9f8251d>
2008-04-07 08:01:17 +0000

    Nuevos archivos

---
+++ ProyectoLenguajes2/src/Misc.java
@@ -0,0 +1,20 @@
+/*
+ * Misc.java
+ *
+ * Created on April 5, 2008, 10:50 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jamil
+ */
+public class Misc {
+    
+    /** Creates a new instance of Misc */
+    public static Bloque acumulador;
+    java.util.List l;
+    
+}
---
+++ ProyectoLenguajes2/src/Tipo.java
@@ -0,0 +1,57 @@
+/*
+ * Tipo.java
+ *
+ * Created on April 6, 2008, 4:04 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jamil
+ */
+public abstract class Tipo {
+    
+    
+}
+
+class Arreglo extends Tipo{
+    private int size;
+    private Tipo tipo;
+    
+    public Arreglo(int n, Tipo t){
+        this.size = n;
+        this.tipo = t;
+    }
+}
+
+class TBasico extends Tipo{
+    private TipoB tipo;
+    
+    public TBasico(TipoB t){
+        this.tipo = t;
+    }
+}
+
+enum TipoB{
+    ENTERO,
+    REAL,
+    BOOLEANO
+}
+
+enum TipoES{
+    IN,
+    OUT,
+    IN_OUT
+}
+
+class TParam extends Tipo{
+    private Tipo tipo;
+    private TipoES es;
+    
+    public TParam(Tipo t, TipoES e){
+        this.tipo = t;
+        this.es = e;
+    }
+}
